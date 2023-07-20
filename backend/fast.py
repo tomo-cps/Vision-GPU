7c20ff284c3885e06f19414ceca2a247de97ca68
tomo-cps <tomoaki.ohkawa@cps.akita-pu.ac.jp>
2023-07-21 03:58:13 +0900

    add

--- backend/fast.py
+++ backend/fast.py
@@ -2,13 +2,26 @@
 from fastapi import FastAPI
 import pynvml
 import socket
-import os
+import pexpect
+import subprocess
 
 hostname = socket.gethostname()
 print("マシンのホスト名:", hostname)
 
-username = os.getlogin()
-print("Logged-in username:", username)
+child = pexpect.spawn("who")
+child.expect(pexpect.EOF)
+detailuser = child.before.decode().strip()
+print(detailuser)
+if detailuser:
+    username = detailuser.split("\n")
+    username = username[0].split(" ")
+    username = username[0]
+else:
+    username=None
+
+result = subprocess.check_output("who")
+detailuser = result.decode().strip().split("\n")
+print(detailuser)
 
 app = FastAPI()
 
@@ -34,22 +47,35 @@ async def get_gpu_info():
         gpu_data = {}
         handle = pynvml.nvmlDeviceGetHandleByIndex(i)
         gpu_name = pynvml.nvmlDeviceGetName(handle)
+
+
         memory_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
+        total_memory = memory_info.total // (1024 * 1024)
+        used_memory = memory_info.used // (1024 * 1024)
+        memory_utilization = (used_memory / total_memory) * 100
+
+
+
         utilization = pynvml.nvmlDeviceGetUtilizationRates(handle)
         fan_speed = pynvml.nvmlDeviceGetFanSpeed(handle)
         temperature = pynvml.nvmlDeviceGetTemperature(handle, pynvml.NVML_TEMPERATURE_GPU)
 
         gpu_data["MachineName"] = hostname
         gpu_data["LoginUser"] = username
+        gpu_data["DetailUser"] = detailuser
         gpu_data["GPU"] = f"GPU{i}"
         gpu_data["Name"] = gpu_name
-        gpu_data["Memory"] = f"{memory_info.total / 1024**2} MB"
+
+        gpu_data["TotalMemory"] = f"{total_memory} MB"
+        gpu_data["UsedMemory"] = f"{used_memory}MB/{total_memory} MB"
+        gpu_data["MemoryUtilization"] = f"{memory_utilization:.2f}%"
+
+
         gpu_data["Utilization"] = f"{utilization.gpu}%"
         gpu_data["FanSpeed"] = f"{fan_speed}%"
         gpu_data["Temperature"] = f"{temperature}°C"
         # gpu_data["ImgPath"] = "../assets/"+hostname+".png"
-        gpu_data["ImgPath"] = "../assets/"+hostname+".png"
-
+        gpu_data["ImgPath"] = f"../assets/"+hostname+".png"
 
         gpu_info.append(gpu_data)
         print(gpu_info)
